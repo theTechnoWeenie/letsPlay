da0b1ef02876038b24042d5c01fb6148495dc36d
Sam Jantz <sjantz0@gmail.com>
2015-02-28 22:15:12 -0600

    Made api more consistent, and validates steam_id

--- src/api.py
+++ src/api.py
@@ -1,4 +1,4 @@
-import requests, lxml.html
+import requests, lxml.html, re
 from lxml import etree
 from flask import Flask, request, jsonify
 from caches.Cache import Cache
@@ -21,7 +21,7 @@ def get_steam_id():
     r = requests.get(endpoint)
     response = r.json()["response"]
     if response["success"] is not 1:
-        return create_error_json('A user with url %s was not found'%vanity_url)
+        return create_error_json('A user with url %s was not found (message: %s)'%(vanity_url, response["message"]))
     return jsonify({'steam_id':response['steamid'], 'success':True})
 
 def get_friend_list():
@@ -29,11 +29,12 @@ def get_friend_list():
     Gets the steam_ids of all friends associated with steam_id
     """
     steam_id = request.args.get('steam_id')
-    if steam_id is None:
-        return create_error_json("steam_id must be supplied")
+    errors = find_invalid(steam_id)
+    if errors is not None:
+        return errors
     endpoint = 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=%s&steamid=%s&relationship=friend'%(API_KEY, steam_id)
     raw_response = requests.get(endpoint)
-    friends = {'friends': [{'steam_id':x['steamid'], 'since': x['friend_since']} for x in raw_response.json()['friendslist']['friends']]}
+    friends = {'friends': [{'steam_id':x['steamid'], 'since': x['friend_since']} for x in raw_response.json()['friendslist']['friends']], "success":True}
     return jsonify(friends)
 
 def get_games():
@@ -41,6 +42,9 @@ def get_games():
     Gets all game appid's associated with steam_id
     """
     steam_id = request.args.get('steam_id')
+    errors = find_invalid(steam_id)
+    if errors is not None:
+        return errors
     return jsonify(get_games_request(steam_id))
 
 
@@ -49,17 +53,17 @@ def get_games_request(steam_id):
     Performs the REST request to get the games of user <steam_id>
     Returns the json for total games, and a list of games and forever_playtime
     """
-    if steam_id is None:
-        return create_error_json('steam_id must be provided')
-    cached = GAME_LIBRARY_CACHE.get(steam_id)
-    if cached is None:
+    all_games = GAME_LIBRARY_CACHE.get(steam_id)
+    if all_games is None:
         endpoint = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json"%(API_KEY, steam_id)
         raw_response = requests.get(endpoint)
         if raw_response.status_code is not 200:
             return create_error_json('There was an unexpected api error'), raw_response.status_code
-        cached = raw_response.json()["response"]
-        GAME_LIBRARY_CACHE.set(steam_id, cached)
-    return cached
+        all_games = raw_response.json()["response"]
+        all_games['success'] = True
+        all_games["games"] = [{"app_id":game["appid"], "playtime_forever":game["playtime_forever"]} for game in all_games["games"]]
+        GAME_LIBRARY_CACHE.set(steam_id, all_games)
+    return all_games
 
 def get_info_for_game():
     """
@@ -86,3 +90,16 @@ def create_error_json(message):
     Boiler plate for creating an error json blob.
     """
     return jsonify({'message':message,'success':False})
+
+def find_invalid(steam_id):
+    """
+    Validates a steam id. Returns None if everything is okay, or error json if somethign is awry.
+    """
+    if steam_id is None:
+        return create_error_json('Steam id must be provided.')
+    if len(steam_id) is not 17:
+        return create_error_json('Steam id is the incorrect length (%d) must be 17 numbers'%len(steam_id))
+    m = re.search('7656119[0-9]{10}$', steam_id)
+    if m is None:
+        return create_error_json('Steam id (%s) is not numeric only.'%steam_id)
+    return None
